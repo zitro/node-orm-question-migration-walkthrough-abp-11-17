a0867d0ea66e559de149bcb430b2f108d5556ecd
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-16 16:39:34 +0000

    Done.

--- models/Question.js
+++ models/Question.js
@@ -3,8 +3,14 @@
 const db = require("../config/db")
 
 class Question{
-
+  static CreateTable() {
+    return new Promise(function(resolve){
+      const sql = `CREATE TABLE questions (id INTEGER PRIMARY KEY, content TEXT)`
+      db.run(sql, function(){
+        resolve("questions table created")
+      })
+    })
+  }
 }
-
 
 module.exports = Question;
