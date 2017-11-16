14fdb63de7ad57f7f64506757a534adaf4211da1
aviflombaum <avi@flombaum.com>
2017-11-16 00:22:30 -0500

    Solved:

--- models/Question.js
+++ models/Question.js
@@ -3,7 +3,18 @@
 const db = require("../config/db")
 
 class Question{
-
+  static CreateTable() {
+    return new Promise(function(resolve){
+      const sql = `CREATE TABLE questions (
+        id INTEGER PRIMARY KEY,
+        content TEXT
+      )`
+      
+      db.run(sql, function(){
+        resolve("questions table created")
+      })      
+    })
+  }
 }
 
 
