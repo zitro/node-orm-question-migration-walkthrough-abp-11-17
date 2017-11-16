1d6ab12240a53f4e79b906cf655621576bf3557a
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-16 16:37:33 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -3,8 +3,16 @@
 const db = require("../config/db")
 
 class Question{
-
+  static CreateTable(){
+    return new Promise(function(resolve))
+    {
+      const sql = `CREATE TABLE
+  questions (id INTEGER PRIMARY KEY)'
+    db.run(sql, function(){
+      resolve('quesitons table created')
+    })      
+    })
+  }
 }
-
 
 module.exports = Question;
