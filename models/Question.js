177ad97138fe35e913241f42ec4c5fe8c8e1d640
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-16 16:39:23 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -5,7 +5,7 @@ const db = require("../config/db")
 class Question{
   static CreateTable() {
     return new Promise(function(resolve){
-      const sql = `CREATE TABLE questions (id INTEGER PRIMARY KEY)`
+      const sql = `CREATE TABLE questions (id INTEGER PRIMARY KEY, content TEXT)`
       db.run(sql, function(){
         resolve("questions table created")
       })
